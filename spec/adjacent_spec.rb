4eb9d8c8aa85d534ea0bfb6cab3d329f7703685a
Duy Khoa <duykhoa12t@gmail.com>
2016-06-11 15:06:07 +0800

    More test

--- spec/adjacent_spec.rb
+++ spec/adjacent_spec.rb
@@ -1,25 +1,55 @@
 require_relative './spec_helper'
 
 describe Al::Adjacent do
-  let(:point) { Al::Point.new(0, 0) }
 
   describe "#for" do
-    let(:adjacent) { Al::Adjacent.for(point) }
+    describe "point is 0, 0" do
+      let(:point) { Al::Point.new(0, 0) }
+      let(:adjacent) { Al::Adjacent.for(point) }
 
-    it "must include 1, 0" do
-      adjacent.must_include Al::Point.new(1, 0)
+      it "must include 1, 0" do
+        adjacent.must_include Al::Point.new(1, 0)
+      end
+
+      it "must include 0, 1" do
+        adjacent.must_include Al::Point.new(0, 1)
+      end
+
+      it "wont include 0, -1" do
+        adjacent.wont_include Al::Point.new(0, -1)
+      end
+
+      it "wont include -1, 0" do
+        adjacent.wont_include Al::Point.new(-1, 0)
+      end
+
+      it "only contains 2 adjacent" do
+        adjacent.size.must_equal 2
+      end
     end
 
-    it "must include 0, 1" do
-      adjacent.must_include Al::Point.new(0, 1)
-    end
+   describe "point is 1, 1" do
+      let(:point) { Al::Point.new(1, 1) }
+      let(:adjacent) { Al::Adjacent.for(point) }
 
-    it "wont include 0, -1" do
-      adjacent.must_include Al::Point.new(0, 1)
-    end
+      it "must include 1, 0" do
+        adjacent.must_include Al::Point.new(1, 0)
+      end
 
-    it "wont include -1, 0" do
-      adjacent.must_include Al::Point.new(0, 1)
+      it "must include 0, 1" do
+        adjacent.must_include Al::Point.new(0, 1)
+      end
+
+      it "must include -1, 0" do
+        adjacent.must_include Al::Point.new(2, 1)
+      end
+      it "must include 0, -1" do
+        adjacent.must_include Al::Point.new(1, 2)
+      end
+
+      it "returns 4 adj. points" do
+        adjacent.size.must_equal 4
+      end
     end
   end
 end
