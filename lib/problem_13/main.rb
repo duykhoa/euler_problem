cac681a9987d3761ef1cc8f64d5582c4433a990f
Duy Khoa <duykhoa12t@gmail.com>
2016-06-27 01:45:19 +0800

    Solve problem 13

--- lib/problem_13/main.rb
+++ lib/problem_13/main.rb
@@ -2,7 +2,18 @@ require_relative './plus'
 
 module Problem_13
   class Main
-    def solve(num_list)
+    def solve(*num_list)
+      num1, *tail = num_list
+      return num1 if tail.empty?
+
+      plus_caller.call(
+        num1,
+        solve(*tail)
+      )
+    end
+
+    def plus_caller
+      @plus_caller ||= Plus::new
     end
   end
 end
--- spec/problem_13/main_spec.rb
+++ spec/problem_13/main_spec.rb
@@ -1,5 +1,30 @@
 require_relative '../spec_helper'
 
 describe Problem_13::Main do
+  let(:solver) { Problem_13::Main.new }
 
+  it do
+    solver.solve("1").must_equal "1"
+  end
+
+  it do
+    solver.solve("1", "2").must_equal "3"
+  end
+
+  it do
+    solver.solve(
+      "1001",
+      "1001"
+    ).must_equal "2002"
+  end
+
+  it do
+    solver.solve(
+      "1001",
+      "1001",
+      "1001",
+      "1001",
+      "1001"
+    ).must_equal "5005"
+  end
 end
